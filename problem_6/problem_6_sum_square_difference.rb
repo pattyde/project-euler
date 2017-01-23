c27a20f2aa206ce35f71956207b83a3d19284f58
Patrick De Young <jpdeyoung@gmail.com>
2017-01-23 19:30:11 +1100

    finish problem 6 in ruby

--- problem_6/problem_6_sum_square_difference.rb
+++ problem_6/problem_6_sum_square_difference.rb
@@ -1,12 +1,23 @@
 class SumSquareDifference
 
-  def list_of_numbers
-    (1..100).to_a
+  def difference
+    square_of_sum - sum_of_squares
   end
 
-  def sum_of_squares
-    squares = list_of_numbers.map { |n| n * n }
-    
-  end
+  private
+
+    def list_of_numbers
+      (1..100).to_a
+    end
+
+    def sum_of_squares
+      list_of_numbers.map { |n| n * n }
+                     .reduce(:+)
+    end
+
+    def square_of_sum
+      sum = list_of_numbers.reduce(:+)
+      square = sum * sum
+    end
 
 end
